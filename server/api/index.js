7d4d5bdec97206960b99ca19d0087b1835d4dd6f
Chris Edwards <cedwards@reisystems.com>
2015-12-06 17:19:52 -0500

    add sql parse lib

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const SqlHelper = require('../../lib/api/sql');
+
 exports.register = function (server, options, next) {
 
     server.route({
@@ -11,6 +13,20 @@ exports.register = function (server, options, next) {
         }
     });
 
+    server.route({
+        method: 'GET',
+        path: '/search/{index}',
+        handler: function (request, reply) {
+
+            reply(
+                {
+                    index: request.params.index,
+                    query: request.query,
+                    sql: SqlHelper.getString(request.query)
+                }
+            );
+        }
+    });
 
     next();
 };
