fe3ea5d2848ab820d2eb8fcccde50309e6af8147
Chris Edwards <cedwards@reisystems.com>
2015-12-07 14:49:08 -0500

    testing out sql proxy

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const SqlHelper = require('../../lib/api/sql');
+const Wreck = require('wreck');
+
 
 exports.register = function (server, options, next) {
 
@@ -15,16 +17,35 @@ exports.register = function (server, options, next) {
 
     server.route({
         method: 'GET',
-        path: '/search/{index}',
+        path: '/search/{source}',
         handler: function (request, reply) {
+
+            var params = request.query;
+            params.source = request.params.source;
 
             reply(
                 {
-                    index: request.params.index,
-                    query: request.query,
-                    sql: SqlHelper.getString(request.query)
+                    sql: SqlHelper.getString(params)
                 }
             );
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/sql',
+        config: {
+
+        },
+        handler: function (request, reply) {
+
+            var params = request.query;
+
+            Wreck.get('http://127.0.0.1:9200/_sql?sql='+params['q'], function (err, res, payload) {
+
+                reply(err,payload.toString()).type('application/json');
+
+            });
         }
     });
 
